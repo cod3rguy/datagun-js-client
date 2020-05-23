31f424a1633bd3f2b2ec931f4e459bcdd948650f
Sean Mishra <email@santakmishra.com>
2020-05-23 11:01:48 -0500

    working basic implementation with example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-const datagun = () => console.log('test')
+import io from 'socket.io-client'
+
+const targets = {}
+
+const datagun = {
+  initialize: (apiKey) => { console.log('Datagun initialized.') },
+  paintTarget: (targetName, hitCallback) => {
+    targets[targetName] = hitCallback
+  }
+}
+
+const socket = io('http://863394de.ngrok.io')
+socket.on('connect', () => console.log('connected'))
+socket.on('event', (data) => {
+  console.log(data)
+  if (targets[data.target]) targets[data.target](data.payload)
+})
+socket.on('disconnect', () => console.log('disconnected'))
 
 if (window) window.datagun = datagun
 
